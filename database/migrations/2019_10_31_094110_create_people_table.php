23d879c47db966025e897f4edbf1c1f80b4132a6
samboosa5k <43879542+samboosa5k@users.noreply.github.com>
2019-10-31 11:12:23 +0100

    migrations etc done

---
+++ database/migrations/2019_10_31_094110_create_people_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePeopleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->nullable();
+            $table->string('title')->nullable();
+            $table->unsignedTinyInteger('age')->nullable();
+            $table->string('born')->nullable();
+            $table->string('died')->nullable();
+            $table->string('features')->nullable();
+            $table->string('hair_color')->nullable();
+            $table->string('eye_color')->nullable();
+            $table->unsignedSmallInteger('height')->nullable();
+            $table->unsignedSmallInteger('weight')->nullable();
+            $table->string('nationality')->nullable();
+            $table->string('occupation')->nullable();
+            $table->string('status_text')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('people');
+    }
+}
